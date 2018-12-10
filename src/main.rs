dfef49a4562d62c12cfcf307e91d8ca62657cf6a
Tony Hammond <tony@tonyhammond.org>
2018-12-10 12:38:57 +0000

    Added a config file: Settings.toml

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-extern crate csv;
-
+use config::{Config, File, FileFormat};
+use csv::Reader;
 use std::error::Error;
-use std::io;
 use std::process;
 
 fn example() -> Result<(), Box<Error>> {
-    // Build the CSV reader and iterate over each record.
-    let mut rdr = csv::Reader::from_reader(io::stdin());
+    let mut c = Config::new();
+
+    c.merge(File::new("Settings", FileFormat::Toml).required(false))
+        .unwrap();
+    let csv_path = c.get_str("csv_path").unwrap();
+    println!("csv_path = {:?}", csv_path);
+
+    println!("csv recs = ");
+    let mut rdr = Reader::from_path(csv_path)?;
     for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here..
         let record = result?;
         println!("{:?}", record);
     }
+
     Ok(())
 }
 

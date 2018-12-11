b8493f8dffd90d175f2d808ae38a0e12043c48f7
Tony Hammond <tony@tonyhammond.org>
2018-12-11 15:25:05 +0000

    Tweaked README

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ The `bucket` crate is just a playground for Rust experiments.
 Initially we're going to use the `csv` crate to read from a `.csv` file. We'll use
 [csv/test.csv](../../../csv/test.csv) for now.
 
+We're also going to experiment with file organization and module structure,
+and with documenting the code.
 
-```
+
+```csv
 name,population,latitude,longitude
 New York ,8287238,40.7305991,-73.9865812
 Los Angeles ,3826423,34.053717,-118.2427266
